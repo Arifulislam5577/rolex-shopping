8e827f2189d790ccb539f78abd8bd79a7f06d6c5
Arif5577 <arifalamm33@gmail.com>
2022-02-15 16:01:51 +0600

    new bug

--- app.js
+++ app.js
@@ -27,18 +27,9 @@ mongoose.connect(process.env.MONGODB_URI, () => {
 });
 
 // --------------------------deployment------------------------------
-const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
+  app.use(express.static("client/build"));
 }
 // --------------------------deployment------------------------------
 
