b5704a0c8bbd8be3515718391308f0a6ab52e8ac
Arif5577 <arifalamm33@gmail.com>
2022-02-15 18:57:33 +0600

    fixed payment bug

--- client/src/components/Order/OrderDetails.js
+++ client/src/components/Order/OrderDetails.js
@@ -48,7 +48,7 @@ const OrderDetails = () => {
   const makeRequest = async () => {
     try {
       const { data } = await axios.post(
-        "http://localhost:5000/api/v1/payment",
+        "/api/v1/payment",
         {
           tokenId: Stripetoken.id,
           amount: orderCalculation?.totalPrice,
