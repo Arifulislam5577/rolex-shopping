4694a4219e276b5099337ebe92342cff23064e32
Arif5577 <arifalamm33@gmail.com>
2022-12-04 12:34:43 +0600

    client product details page edited

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-light py-5 border border-top-2 shadow-lg">
+    <footer className="bg-light py-5 border border-top-2 shadow-lg mt-auto">
       <div className="container">
         <div className="row">
           <div className="col-6 col-sm-6 col-md-6 col-lg-4">
